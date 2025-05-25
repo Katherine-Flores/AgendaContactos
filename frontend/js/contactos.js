e08b569532acc9ba0d88395f406602b71c555de4
Katherine-Flores <katherinef@mxxfce>
2025-05-24 20:16:47 -0600

    validacion de input correo

--- frontend/js/contactos.js
+++ frontend/js/contactos.js
@@ -66,70 +66,6 @@ function renderizarPaginacion(totalContactos) {
   }
 }
 
-/*async function cargarContactos() {
-  const token = localStorage.getItem('token');
-  if (!token) return;
-
-  try {
-    const res = await fetch('http://localhost:3000/api/dashboard/contactos', {
-      headers: {
-        Authorization: 'Bearer ' + token
-      }
-    });
-
-    const json = await res.json();
-
-    if (!res.ok) {
-      console.error(json);
-      Swal.fire('Error', 'No se pudieron cargar los contactos', 'error');
-      return;
-    }
-
-    console.log(json);
-    const contactos = json;
-
-    const tbody = document.querySelector('#tablaContactos tbody');
-    tbody.innerHTML = '';
-
-    contactos.forEach(contacto => {
-      const nombreCompleto = [contacto.primer_nombre, contacto.segundo_nombre, contacto.primer_apellido, contacto.segundo_apellido]
-        .filter(n => n)
-        .join(' ') || '-';
-
-      const telefono = contacto.telefono || '-';
-      const correo = contacto.correo_electronico || '-';
-      const categorias = contacto.categorias.length > 0
-        ? contacto.categorias.map(cat => `<span class="badge bg-info text-dark me-1">${cat}</span>`).join('')
-        : '-';
-
-      const tr = document.createElement('tr');
-      tr.innerHTML = `
-        <td>${nombreCompleto}</td>
-        <td>${telefono}</td>
-        <td>${correo}</td>
-        <td>${categorias}</td>
-        <td>
-          <i class="fa-solid fa-pen-to-square editar btn btn-warning" style="cursor:pointer;"></i>
-          <i class="fa-solid fa-trash eliminar btn btn-danger" style="cursor:pointer;"></i>
-        </td>
-      `;
-
-      tr.querySelector('.editar').addEventListener('click', () => {
-        editarContacto(contacto);
-      });
-
-      tr.querySelector('.eliminar').addEventListener('click', () => {
-        eliminarContacto(contacto.id);
-      });
-
-      tbody.appendChild(tr);
-    });
-  } catch (err) {
-    console.error('Error de red o JSON:', err);
-    Swal.fire('Error', 'Ocurrió un error al cargar los contactos', 'error');
-  }
-}*/
-
 let contactos = []; // Para guardar la lista original (útil para filtros)
 
 async function cargarContactos() {
@@ -219,9 +155,16 @@ document.getElementById('btnNuevoContacto').addEventListener('click', async () =
     showCancelButton: true,
     preConfirm: () => {
       const getValue = id => document.getElementById(id).value.trim();
+      const correo = getValue('correo');
       const categoriasSeleccionadas = Array.from(document.querySelectorAll('.categoria-checkbox:checked'))
         .map(cb => parseInt(cb.value));
 
+        const correoValido = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+        if (!correoValido.test(correo)) {
+          Swal.showValidationMessage('El correo electrónico no es válido');
+          return false;
+        }
+          
       return {
         primer_nombre: getValue('primer_nombre'),
         segundo_nombre: getValue('segundo_nombre'),
@@ -345,8 +288,14 @@ async function editarContacto(contacto) {
     cancelButtonText: 'Cancelar',
     preConfirm: () => {
       const getValue = id => document.getElementById(id).value.trim();
+      const correo = getValue('correo')
       const categoriasSeleccionadas = Array.from(document.querySelectorAll('.categoria-checkbox:checked'))
         .map(cb => parseInt(cb.value));
+        const correoValido = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+        if (!correoValido.test(correo)) {
+          Swal.showValidationMessage('El correo electrónico no es válido');
+          return false;
+        }
 
       return {
         primer_nombre: getValue('primer_nombre'),
